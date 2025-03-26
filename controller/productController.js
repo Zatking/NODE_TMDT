7fb693812e0ad8a47ba1fa75f2985c7df1c1b2b6
Zatking <DVT41557@gmail.com>
2025-03-26 21:52:37 +0700

    update delete

--- controller/productController.js
+++ controller/productController.js
@@ -274,11 +274,11 @@ const updateProduct = async (req, res) => {
 
 const deleteProduct = async (req, res) => {
   try {
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findByIdAndDelete(req.params.id);
     if (!product) {
       return res.status(404).json({ error: "Sản phẩm không tồn tại" });
     }
-    await product.delete();
+    
     res.status(200).json({ message: "Xóa sản phẩm thành công" });
   } catch (error) {
     console.log("Lỗi khi xóa sản phẩm: ", error);
